ba8b15697b0f98a807c636cbc818b6500679fe35
KirillTsyplyaev <159521559+KirillTsyplyaev@users.noreply.github.com>
2024-05-24 12:27:03 +0300

    Контрольная 1 вариант 3

---
+++ контрольная второе задание.cs
@@ -0,0 +1,150 @@
+﻿using System;
+
+
+namespace cr
+{
+
+
+    public abstract class Shape
+    {
+        public abstract double CalculateVolume();
+        public abstract void DisplayInfo();
+    }
+
+    public class Sphere : Shape
+    {
+        private double radius;
+
+        public Sphere(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public override double CalculateVolume()
+        {
+            return (4.0 / 3.0) * Math.PI * Math.Pow(radius, 3);
+        }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"Сфера с радиусом {radius}");
+        }
+    }
+
+
+    public class Cube : Shape
+    {
+        private double sideLength;
+
+        public Cube(double sideLength)
+        {
+            this.sideLength = sideLength;
+        }
+
+        public override double CalculateVolume()
+        {
+            return Math.Pow(sideLength, 3);
+        }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"Куб со стороной {sideLength}");
+        }
+    }
+
+
+    public class Cylinder : Shape
+    {
+        private double radius;
+        private double height;
+
+        public Cylinder(double radius, double height)
+        {
+            this.radius = radius;
+            this.height = height;
+        }
+
+        public override double CalculateVolume()
+        {
+            return Math.PI * Math.Pow(radius, 2) * height;
+        }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"Цилиндр с радиусом {radius} и высотой {height}");
+        }
+    }
+
+    class Program
+    {
+        
+
+
+            static void Main(string[] args)
+            {
+                Sphere[] spheres = {
+                    new Sphere(3),
+                    new Sphere(2),
+                    new Sphere(4),
+                    new Sphere(5),
+                    new Sphere(6)
+                };
+
+                Cube[] cubes = {
+                    new Cube(2),
+                    new Cube(1),
+                    new Cube(3),
+                    new Cube(2),
+                    new Cube(5),
+
+                };
+
+                Cylinder[] cylinders = {
+                    new Cylinder(2, 4),
+                    new Cylinder(1, 3),
+                    new Cylinder(3, 5),
+                    new Cylinder(3, 5),
+                    new Cylinder(3, 8)
+                };
+
+                Console.WriteLine("кубы");
+
+                foreach (Cube sp in cubes)
+                {
+                    sp.DisplayInfo();
+                    double volume = sp.CalculateVolume();
+                    Console.WriteLine($"Обьем {volume}");
+
+                }
+                Console.WriteLine("Сферы");
+                foreach (Sphere sp in spheres)
+                {
+                    sp.DisplayInfo();
+                    double volume = sp.CalculateVolume();
+                    Console.WriteLine($"Обьем {volume}");
+
+                }
+
+                foreach (Cylinder sp in cylinders)
+                {
+                    sp.DisplayInfo();
+                    double volume = sp.CalculateVolume();
+                    Console.WriteLine($"Обьем {volume}");
+
+                }
+
+
+                Shape[] allShapes;
+
+
+
+
+
+
+            }
+        
+    }
+}
+
+    
+
---
+++ контрольная первое задание.cs
@@ -0,0 +1,107 @@
+﻿using System.Numerics;
+
+internal class Program
+{
+    static void Main(String[] args)
+    {
+        Dot[] dots = 
+            { 
+            new Dot(1, 2, 3),
+            new Dot(2,3,5), 
+            new Dot (2,3,4),
+            new Dot(6,3,1) 
+        };
+        Vector[] vector = 
+        {
+            new Vector(dots[0], dots[1]), 
+            new Vector(dots[0], dots[2]), 
+            new Vector(dots[2], dots[3]),
+            new Vector(dots[1], dots[2]),
+            new Vector(dots[2], dots[1])
+        };
+        foreach(Vector vec in vector)
+        {
+            Vector.VectorInfo(vec); double length = Vector.VectorLength(vec);
+            Console.WriteLine($"длина вектора {length}");
+        }
+        
+        
+       
+    }
+}
+struct Vector
+{
+    private Dot _dot1;
+    private Dot _dot2;
+   
+   
+    public Vector(Dot dot1, Dot dot2)
+    {
+        _dot1 = dot1;
+        _dot2 = dot2;
+    }
+
+    public Dot Dot1
+    {
+        get { return _dot1; }
+        private set { _dot1 = value; }
+    }
+
+    public Dot Dot2
+    {
+        get { return _dot2; }
+        private set { _dot2 = value; }
+    }
+
+    public static double VectorLength(Vector vector)
+    {
+        double lengthOfVector = Math.Sqrt(Math.Pow(vector._dot2.X - vector._dot1.X, 2) + Math.Pow(vector._dot2.Y - vector._dot1.Y, 2) + Math.Pow(vector._dot2.Z - vector._dot1.Z, 2));
+        return lengthOfVector;
+    }
+
+    public static void VectorInfo(Vector vector)
+    {
+        Console.WriteLine($" координаты первой точки {vector._dot1.X},{vector._dot1.Y}, {vector._dot1.Z}  координаты второй точки{vector._dot2.X},{vector._dot2.Y}, {vector._dot2.Z},  ");
+    }
+}
+struct Dot
+{
+    
+    private int[] dot = new int[3] ;
+
+    public Dot(int x, int y, int z)
+    {
+        dot[0] = x;
+        dot[1] = y;
+        dot[2] = z;
+    }
+
+    public int X
+    {
+        get { return dot[0]; }
+        private set { dot[0] = value; }
+    }
+
+    public int Y
+    {
+        get { return dot[1]; }
+        private set { dot[1] = value; }
+    }
+
+    public int Z
+    {
+        get { return dot[2]; }
+        private set { dot[2] = value; }
+    }
+
+    public int[,] returnMatrix(Dot dot1, Dot dot2)
+    {
+        int[,] Matrix = new int[2, 3]
+        {
+            { dot1.X, dot1.Y, dot1.Z},
+            { dot2.X, dot2.Y,dot2.Z},
+        };
+        return Matrix;
+    }
+
+}
